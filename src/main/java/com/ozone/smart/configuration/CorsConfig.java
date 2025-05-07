518ac2fbcd335e2daa661ac2b698e10d4e44921d
vasanthk18 <vasanthkrml@gmail.com>
2025-05-08 01:57:07 +0530

    made changes in cors file

--- src/main/java/com/ozone/smart/configuration/CorsConfig.java
+++ src/main/java/com/ozone/smart/configuration/CorsConfig.java
@@ -20,12 +20,10 @@ public class CorsConfig implements WebMvcConfigurer {
 	
 	    @Override
 	    public void addCorsMappings(CorsRegistry registry) {
-	        registry.addMapping("/**")
-	                .allowedOrigins("https://vby.vercel.app", "http://localhost:5173") // Replace with your React frontend URL
-	                // .allowedOriginPatterns("*")
-					.allowedMethods("GET", "POST", "PUT", "DELETE","OPTIONS")
-	                .allowedHeaders("*")
-	                .allowCredentials(true);
+	      registry.addMapping("/**")
+        .allowedOrigins("http://localhost:5173")
+        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+        .allowedHeaders("*");
 //	        		.exposedHeaders("X-Message");
 	    }
 	   
